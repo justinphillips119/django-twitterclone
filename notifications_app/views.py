492fa091cb6338f94ebce0189f4b6291e2f3ecbf
Justin_Phillips <dcoltwinchester1967@gmail.com>
2020-09-15 22:00:40 -0400

    class view conversions

--- notifications_app/views.py
+++ notifications_app/views.py
@@ -3,35 +3,38 @@ from twitter_user.models import TwitterUser
 from tweet_app.models import Tweet
 from notifications_app.models import Notification
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 
 
 
-@login_required
-def notification_view(request, user_id):
-    tweets = Notification.objects.filter(tagged=request.user)
-    count = 0
-    notification_list = []
-    for tweet in tweets:
-        if tweet.read == False:
-            count += 1
-            notification_list.append(tweet.tagged_tweet)
-            tweet.read = True
-            tweet.save()
-    return render(request, "notifications.html", {"tagged": notification_list, "count": count})
-
-
-
-def notification_count_view(request):
-    user = request.user
-    if user.is_authenticated:
-        notifications = Notification.objects.filter(tagged=user)
+class NotificationView(LoginRequiredMixin, TemplateView):
+    def get(self, request, user_id):
+        tweets = Notification.objects.filter(tagged=request.user)
         count = 0
-        for notification in notifications:
-            if notification.read == False:
+        notification_list = []
+        for tweet in tweets:
+            if tweet.read == False:
                 count += 1
-    else:
-        count = 0
-    return count
+                notification_list.append(tweet.tagged_tweet)
+                tweet.read = True
+                tweet.save()
+        return render(request, "notifications.html", {"tagged": notification_list, "count": count})
+
+
+
+class NotificationCountView(TemplateView):
+    def get(self, request):
+        user = request.user
+        if user.is_authenticated:
+            notifications = Notification.objects.filter(tagged=user)
+            count = 0
+            for notification in notifications:
+                if notification.read == False:
+                    count += 1
+        else:
+            count = 0
+        return count
 
 
 
--- tweet_app/views.py
+++ tweet_app/views.py
@@ -3,9 +3,11 @@ from twitter_user.models import TwitterUser
 from tweet_app.models import Tweet
 from tweet_app.forms import TweetForm
 from notifications_app.models import Notification
-from notifications_app.views import notification_view, notification_count_view
+from notifications_app.views import NotificationView, NotificationCountView
 from django.contrib.auth.decorators import login_required
 import re
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 
 
 
@@ -18,37 +20,39 @@ def homepage_view(request):
 def user_feed_view(request):
     user = Tweet.objects.filter(user=request.user)
     following = Tweet.objects.filter(user__in=request.user.following.all())
-    notifications = notification_count_view(request)
+    notifications = NotificationCountView.as_view()
     feed = user | following
     feed = feed.order_by("-time")
     return render(request, "userfeed.html", {"feed": feed, "notifications": notifications})
 
 
 
-@login_required
-def new_tweet_view(request):
-    notifications = notification_count_view(request)
-    if request.method == 'POST':
-        form = TweetForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            post = Tweet.objects.create(
-                tweet=data.get('tweet'),
-                user=request.user,
-            )
-            tags = re.findall(r'@(\w+)', data.get('tweet'))
-            if tags:
-                for tag in tags:
-                    tagged_user = TwitterUser.objects.get(username=tag)
-                    if tagged_user:
-                        Notification.objects.create(
-                            tagged=tagged_user,
-                            tagged_tweet=post
-                        )
-            return HttpResponseRedirect(reverse("userfeed"), {"notifications": notifications})
+class NewTweetView(LoginRequiredMixin, TemplateView):
+    def get(self, request):
+        notifications = NotificationCountView.as_view()
+        form = TweetForm()
+        return render(request, "generic_form.html", {"form": form})
 
-    form = TweetForm()
-    return render(request, "generic_form.html", {"form": form})
+    def post(self,request):
+        notifications = NotificationCountView.as_view()
+        if request.method == 'POST':
+            form = TweetForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                post = Tweet.objects.create(
+                    tweet=data.get('tweet'),
+                    user=request.user,
+                )
+                tags = re.findall(r'@(\w+)', data.get('tweet'))
+                if tags:
+                    for tag in tags:
+                        tagged_user = TwitterUser.objects.get(username=tag)
+                        if tagged_user:
+                            Notification.objects.create(
+                                tagged=tagged_user,
+                                tagged_tweet=post
+                            )
+                return HttpResponseRedirect(reverse("userfeed"), {"notifications": notifications})
 
 
 
@@ -56,7 +60,7 @@ def profile_detail_view(request, username):
     user = TwitterUser.objects.filter(username=username).first()
     tweets = Tweet.objects.filter(user=user).order_by("-time")
     tweet_count = len(Tweet.objects.filter(user=user).all())
-    notifications = notification_count_view(request)
+    notifications = NotificationCountView.as_view()
     following_count = len(user.following.all())
     if request.user.is_authenticated:
         following = request.user.following.all()
@@ -66,23 +70,25 @@ def profile_detail_view(request, username):
 
 
 
-def tweet_detail_view(request, tweet_id):
-    tweet = Tweet.objects.get(id=tweet_id)
-    return render(request, "tweet.html", {"tweet": tweet})
+class TweetDetailView(TemplateView):
+    def get(self, request, tweet_id):
+        tweet = Tweet.objects.get(id=tweet_id)
+        return render(request, "tweet.html", {"tweet": tweet})
 
 
 
-def follow_view(request, username):
-    user = request.user
-    follow_user = TwitterUser.objects.filter(username=username).first()
-    user.following.add(follow_user)
-    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+class FollowView(TemplateView):
+    def get(self, request, username):
+        user = request.user
+        follow_user = TwitterUser.objects.filter(username=username).first()
+        user.following.add(follow_user)
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
 
-def unfollow_view(request, username):
-    user = request.user
-    unfollow_user = TwitterUser.objects.filter(username=username).first()
-    user.following.remove(unfollow_user)
-    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
-
+class UnfollowView(TemplateView):
+    def get(self, request, username):
+        user = request.user
+        unfollow_user = TwitterUser.objects.filter(username=username).first()
+        user.following.remove(unfollow_user)
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
